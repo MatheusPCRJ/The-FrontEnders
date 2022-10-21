91f74061dc6e8a1be640f644962708ac82d3b3b3
Paula Dinucci <paulaldinucci@gmail.com>
2022-10-21 11:40:43 -0300

    Alteração js para CEP
    inserção da API para campo do CEP, ainda é necessário fazer algumas alterações

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,2 +1,27 @@
-let testando = document.querySelector(".olamundo").innerText;
-alert(testando);
+'use strict'
+
+//Cada getElementById corresponde ao ID que vai ficar as informações referentes ao endereço
+
+const preencherFormulario = (endereco) =>{
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+const pesquisarCep = async() => {
+    const cep = document.getElementById('cep'.value);
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+    if (endereco.hasOwnProperty('erro')){
+        document.getElementById('endereco').value = 'CEP Não Encontrado'
+    }else{
+        preencherFormulario(endereco);
+    }
+    
+}
+
+document.getElementById('cep')
+        .addEventListener('focusout',pesquisarCep);
