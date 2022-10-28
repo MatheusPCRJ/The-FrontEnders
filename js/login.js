8893e88d3862e27e3031128edb954dcb1ffb2e3a
Matheus Gomes <112782424+MatheusPCRJ@users.noreply.github.com>
2022-10-28 15:43:27 -0300

    Modificação feita em todas as páginas!
    Modificação  realizada em todas as páginas para padronizar!

--- js/login.js
+++ js/login.js
@@ -58,25 +58,45 @@ $(function(){
 });
 
 
+
+
+// Função para o login
 let email = document.querySelector('#email');
 let senha = document.querySelector('#password');
 let resposta = document.querySelector('#loginInvalido');
 
 document.querySelector('#botaologin').onclick = function(){
 	if(email.value == "teste@gmail.com" && senha.value == "12345"){
-		alert('Login Realizado com sucesso')
-		
-	}
-	 else{
-		resposta.innerHTML=('Login e Senha incorreta, click em <b>"Esqueceu sua Senha?"</b>')
-
 		document.querySelector('#formularioMain').addEventListener("submit", function(event){
 			event.preventDefault();
-		
 		});
+		Swal.fire({
+			position: 'center',
+			icon: 'success',
+			title: 'Login Realizado com Sucesso',
+			showConfirmButton: false,
+			timer: 3500,
+		})
+
+		// perguntar ao professor, sem o preventDefault o Swal.Fire Sweet alert não aparece, pq?
+		
 		
 	}
+	else if(email.value == "" || senha.value == ""){
+		resposta.innerHTML=('Preencha todos os campos')
+		document.querySelector('#formularioMain').addEventListener("submit", function(event){
+			event.preventDefault();
+		});
+	}else{
+		resposta.innerHTML=('Login e Senha incorreta, click em <b>"Esqueceu sua Senha?"</b>')
+		document.querySelector('#formularioMain').addEventListener("submit", function(event){
+			event.preventDefault();
+		});
+	}
 }
+// Função do login acaba aqui.
+
+
 
 // Função para mostrar a senha na caixinha
 let pegaSenha = document.querySelector('#password');
